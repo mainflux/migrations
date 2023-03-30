011c54d6996fed0eb693bdc3ac8ee1344f4dc9b8
rodneyosodo <blackd0t@protonmail.com>
2023-03-30 17:32:27 +0300

    Move Uses To Use Channels
    Signed-off-by: rodneyosodo <blackd0t@protonmail.com>

--- migrate/users/v13/minisdk.go
+++ migrate/users/v13/minisdk.go
@@ -8,6 +8,7 @@ import (
 	mf13users "github.com/mainflux/mainflux/users"
 	mf13postgres "github.com/mainflux/mainflux/users/postgres"
 	"github.com/mainflux/migrations/internal/util"
+	"golang.org/x/sync/errgroup"
 )
 
 var (
@@ -18,45 +19,61 @@ var (
 	retrieveErrString = "error %v occured at offset: %d and total: %d during %s"
 )
 
-// RetrieveUsers retrieves existing users from the database
-func RetrieveUsers(ctx context.Context, db mf13postgres.Database) (mf13users.UserPage, error) {
-	usersPage, err := dbRetrieveUsers(ctx, db, mf13users.PageMetadata{Offset: offset, Limit: limit})
-	if err != nil {
-		return mf13users.UserPage{}, fmt.Errorf(retrieveErrString, err, offset, limit, retrievUsersOps)
-	}
-	o := limit
-	limit = util.UpdateLimit(usersPage.Total)
-	for o < usersPage.Total {
-		ths, err := dbRetrieveUsers(ctx, db, mf13users.PageMetadata{Offset: o, Limit: limit})
-		if err != nil {
-			return mf13users.UserPage{}, fmt.Errorf(retrieveErrString, err, o, limit, retrievUsersOps)
-		}
-		usersPage.Users = append(usersPage.Users, ths.Users...)
-		o = o + limit
-	}
+func RetrieveAndWriteUsers(ctx context.Context, db mf13postgres.Database, filePath string) error {
+	out := make(chan []mf13users.User)
 
-	return usersPage, nil
+	g := new(errgroup.Group)
+
+	g.Go(func() error {
+		return RetrieveUsers(ctx, db, out)
+	})
+	g.Go(func() error {
+		return UsersToCSV(filePath, out)
+	})
+
+	return g.Wait()
+}
+
+// RetrieveUsers retrieves existing users from the database
+func RetrieveUsers(ctx context.Context, db mf13postgres.Database, outusers chan<- []mf13users.User) error {
+	o := offset
+	l := limit
+	for {
+		usersPage, err := dbRetrieveUsers(ctx, db, mf13users.PageMetadata{Offset: o, Limit: l})
+		if err != nil {
+			return fmt.Errorf(retrieveErrString, err, o, l, retrievUsersOps)
+		}
+		outusers <- usersPage.Users
+		if o+l >= usersPage.Total {
+			break
+		}
+
+		o = o + l
+		l = util.UpdateLimit(usersPage.Total)
+	}
+	close(outusers)
+	return nil
 }
 
 // UsersToCSV saves users to the provided csv file
 // The format of the users csv file is ID,Email,Password,Metadata
-func UsersToCSV(filePath string, users []mf13users.User) error {
+func UsersToCSV(filePath string, inusers <-chan []mf13users.User) error {
 	f, err := util.CreateFile(filePath, writeUsersOps)
 	if err != nil {
 		return err
 	}
-
 	w := csv.NewWriter(f)
 
 	records := [][]string{{"ID", "Email", "Password", "Metadata"}}
-	for _, user := range users {
-		metadata, err := util.MetadataToString(user.Metadata)
-		if err != nil {
-			return err
+	for users := range inusers {
+		for _, user := range users {
+			metadata, err := util.MetadataToString(user.Metadata)
+			if err != nil {
+				return err
+			}
+			record := []string{user.ID, user.Email, user.Password, metadata}
+			records = append(records, record)
 		}
-		record := []string{user.ID, user.Email, user.Password, metadata}
-		records = append(records, record)
 	}
-
 	return util.WriteData(w, f, records, writeUsersOps)
 }
