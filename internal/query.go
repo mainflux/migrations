5e367df8a4404d29d5eb03e015c7f805788efb40
rodneyosodo <blackd0t@protonmail.com>
2023-04-06 20:10:23 +0300

    Binary Search Instead of Linear Search
    Signed-off-by: rodneyosodo <blackd0t@protonmail.com>

--- internal/query.go
+++ internal/query.go
@@ -5,17 +5,16 @@ const defLimit = uint64(100)
 // UpdateLimit is a helper function that updates the limit when querying
 // based on the total number of items
 func UpdateLimit(total uint64) uint64 {
-	if total <= 1000 {
+	switch {
+	case total <= 1000:
 		return defLimit
-	}
-	if total <= 10000 {
+	case total <= 10000:
 		return uint64(1000)
-	}
-	if total <= 100000 {
+	case total <= 100000:
 		return uint64(10000)
-	}
-	if total <= 1000000 {
+	case total <= 1000000:
 		return uint64(100000)
+	default:
+		return uint64(1000000)
 	}
-	return uint64(1000000)
 }
